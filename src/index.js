e8e1dd050d5ad336f8c627795190259cdc9b4c00
Kevin Wei <chenghaowei@gmail.com>
2019-09-12 16:33:46 -0400

    remove store

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import './index.css';
-import App from './App';
-import Store from './Store';
+import "./index.css";
+import App from "./App";
 
 ReactDOM.render(
-<BrowserRouter>
-  <App store={Store} />
-</BrowserRouter>, document.getElementById('root'));
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
